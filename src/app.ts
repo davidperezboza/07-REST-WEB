fe2111220bdacd6122bef8c78333cac93d41c7be
davidperezboza <david.perez@babelgroup.com>
2025-03-25 15:12:52 +0100

    Video 191

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    res.write('Hello World!');
-    res.end();
-});
+    //res.writeHead(200, {'Content-type': 'text/html'});
+    //res.write(`<h1>URL: ${req.url}</h1>`);
+    //res.end();
 
+    if(req.url === '/'){
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(htmlFile);
+    }else{
+        res.writeHead(404, {'Content-type': 'text/html'});
+        res.end();
+    };
+
+});
 
 server.listen(8080, () => {
     console.log('Server running on port 8080');
