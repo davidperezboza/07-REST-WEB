40b228d649f5858fda321c902fce517d5f427c52
davidperezboza <david.perez@babelgroup.com>
2025-03-25 15:40:27 +0100

    Video 192

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,22 @@ const server = http.createServer((req, res) => {
         const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
         res.writeHead(200, {'Content-type': 'text/html'});
         res.end(htmlFile);
+        return;
+    };
+
+    if(req.url === '/css/style.css'){
+        const cssStyle = fs.readFileSync('./public/css/style.css', 'utf-8');
+        res.writeHead(200, {'Content-type': 'text/css'});
+        res.end(cssStyle);
+        return;
+    };
+    
+    if(req.url === '/js/app.js'){
+        const jsScript = fs.readFileSync('./public/js/app.js', 'utf-8');
+        res.writeHead(200, {'Content-type': 'text/javascript'});
+        res.end(jsScript);    
     }else{
-        res.writeHead(404, {'Content-type': 'text/html'});
+        res.writeHead(404, {'Content-type': 'text/plain'});
         res.end();
     };
 
