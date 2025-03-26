93aef2b0094abc4e31c7b922081523c0e7d05cff
davidperezboza <david.perez@babelgroup.com>
2025-03-26 12:31:21 +0100

    Video 196

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,22 +1,35 @@
 import express from 'express';
 import path from 'path';
 
+interface Options{
+    port: number;
+    public_path: string;
+};
+
 export class Server {
     private app = express();
+    private readonly port: number;
+    private readonly public_path: string;
 
+    constructor(options: Options) {
+        const {port, public_path} = options;
+        this.port = port;
+        this.public_path = public_path;
+    };
+     
     async start() {
         //*Middleware
         //*Public folder
-        this.app.use(express.static('public'));
+        this.app.use(express.static(this.public_path));
 
         //*Routes
         this.app.get('*', (req, res) => {
-            const indexPath = path.join(__dirname + '../../../public/index.html');
+            const indexPath = path.join(__dirname + `../../../${this.public_path}/index.html`);
             res.sendFile(indexPath);
         });
 
-        this.app.listen(3000, () => {
-            console.log(`Server is running on port ${3000}`);
+        this.app.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
         });
     };
 };
