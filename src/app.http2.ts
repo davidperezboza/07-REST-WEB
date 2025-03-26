e2fd7027c0ebc62baa9863273de8769743e8e665
davidperezboza <david.perez@babelgroup.com>
2025-03-26 11:09:39 +0100

    Video 194

---
+++ src/app.http2.ts
@@ -0,0 +1,40 @@
+import http2 from 'http2';
+import fs from 'fs';
+
+const server = http2.createSecureServer({
+    key: fs.readFileSync('./keys/server.key'),
+    cert: fs.readFileSync('./keys/server.crt'),
+}, (req, res) => {
+    console.log(req.url);
+    //res.writeHead(200, {'Content-type': 'text/html'});
+    //res.write(`<h1>URL: ${req.url}</h1>`);
+    //res.end();
+
+    if(req.url === '/'){
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(htmlFile);
+        return;
+    };
+
+    if(req.url === '/css/style.css'){
+        const cssStyle = fs.readFileSync('./public/css/style.css', 'utf-8');
+        res.writeHead(200, {'Content-type': 'text/css'});
+        res.end(cssStyle);
+        return;
+    };
+    
+    if(req.url === '/js/app.js'){
+        const jsScript = fs.readFileSync('./public/js/app.js', 'utf-8');
+        res.writeHead(200, {'Content-type': 'text/javascript'});
+        res.end(jsScript);    
+    }else{
+        res.writeHead(404, {'Content-type': 'text/plain'});
+        res.end();
+    };
+
+});
+
+server.listen(8080, () => {
+    console.log('Server running on port 8080');
+})
---
+++ src/presentation/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+export class Server {
+    private app = express();
+
+    async start() {
+        //*Middleware
+        //*Public folder
+        this.app.use(express.static('public'));
+
+        this.app.listen(3000, () => {
+            console.log(`Server is running on port ${3000}`);
+        });
+    };
+};
