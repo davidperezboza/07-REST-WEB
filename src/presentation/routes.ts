02618268eb1f7beee4db465cb18412a8d8106b0a
davidperezboza <david.perez@babelgroup.com>
2025-03-26 15:05:50 +0100

    Video 205

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,30 +1,12 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { TodoRoutes } from './todos';
 
 export class AppRoutes {
     static get routes(): Router {
         const router = Router();
 
-         router.get('/api/todos', (req, res) => {
-                    res.json([
-                        {
-                            id: 1,
-                            text: 'Buy milk',
-                            createdAt: new Date(),
-                        },
-                        {
-                            id: 2,
-                            text: 'Buy bread',
-                            createdAt: null,
-                        },
-                        {
-                            id: 3,
-                            text: 'Buy butter',
-                            createdAt: new Date(),
-                        },
-                    ]);
-            });
+        router.use('/api/todos', TodoRoutes.routes);
         
-
         return router
     };
 }
