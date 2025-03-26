75969a5dceb523c16cd19e387541efa57c91358c
davidperezboza <david.perez@babelgroup.com>
2025-03-26 14:43:08 +0100

    Video 204

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response } from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router
     public_path: string;
 }
 
@@ -10,11 +11,13 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly public_path: string;
+    private readonly routes: Router;
 
     constructor(options: Options) {
-        const { port, public_path } = options;
+        const { port, public_path, routes } = options;
         this.port = port;
         this.public_path = public_path;
+        this.routes = routes;
     }
 
     async start() {
@@ -22,29 +25,12 @@ export class Server {
         //* Public folder
         this.app.use(express.static(this.public_path));
 
-        this.app.get('/api/todos', (req: Request, res: Response) => {
-            res.json([
-                {
-                    id: 1,
-                    text: 'Buy milk',
-                    createdAt: new Date(),
-                },
-                {
-                    id: 2,
-                    text: 'Buy bread',
-                    createdAt: null,
-                },
-                {
-                    id: 3,
-                    text: 'Buy butter',
-                    createdAt: new Date(),
-                },
-            ]);
-        });
+        //* Routes
+        this.app.use(this.routes);
 
         //* SPA
-        this.app.get('*', (req: Request, res: Response) => {
-            const indexPath = path.join(__dirname, `../../../${this.public_path}/index.html`);
+        this.app.get('*', (req, res) => {
+            const indexPath = path.join(__dirname, `/../../${this.public_path}/index.html`);
             res.sendFile(indexPath);
         });
 
