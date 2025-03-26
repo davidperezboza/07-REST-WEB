f6fb733e8ca97ec8d54079eb6a073f26b22af360
davidperezboza <david.perez@babelgroup.com>
2025-03-26 14:23:02 +0100

    Video 203

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
 
-interface Options{
+interface Options {
     port: number;
     public_path: string;
-};
+}
 
 export class Server {
     private app = express();
@@ -12,24 +12,44 @@ export class Server {
     private readonly public_path: string;
 
     constructor(options: Options) {
-        const {port, public_path} = options;
+        const { port, public_path } = options;
         this.port = port;
         this.public_path = public_path;
-    };
-     
+    }
+
     async start() {
-        //*Middleware
-        //*Public folder
+        //* Middleware
+        //* Public folder
         this.app.use(express.static(this.public_path));
 
-        //*Routes
-        this.app.get('*', (req, res) => {
-            const indexPath = path.join(__dirname + `../../../${this.public_path}/index.html`);
+        this.app.get('/api/todos', (req: Request, res: Response) => {
+            res.json([
+                {
+                    id: 1,
+                    text: 'Buy milk',
+                    createdAt: new Date(),
+                },
+                {
+                    id: 2,
+                    text: 'Buy bread',
+                    createdAt: null,
+                },
+                {
+                    id: 3,
+                    text: 'Buy butter',
+                    createdAt: new Date(),
+                },
+            ]);
+        });
+
+        //* SPA
+        this.app.get('*', (req: Request, res: Response) => {
+            const indexPath = path.join(__dirname, `../../../${this.public_path}/index.html`);
             res.sendFile(indexPath);
         });
 
         this.app.listen(this.port, () => {
             console.log(`Server is running on port ${this.port}`);
         });
-    };
-};
+    }
+}
