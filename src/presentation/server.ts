91b22c30acaf4999b72cc315933673fef7967821
davidperezboza <david.perez@babelgroup.com>
2025-03-26 11:27:21 +0100

    Video 195

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 export class Server {
     private app = express();
@@ -7,6 +8,12 @@ export class Server {
         //*Middleware
         //*Public folder
         this.app.use(express.static('public'));
+
+        //*Routes
+        this.app.get('*', (req, res) => {
+            const indexPath = path.join(__dirname + '../../../public/index.html');
+            res.sendFile(indexPath);
+        });
 
         this.app.listen(3000, () => {
             console.log(`Server is running on port ${3000}`);
