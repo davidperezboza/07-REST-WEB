64521eedf600e8534f4a73fe3bc582f3c5e0219d
davidperezboza <david.perez@babelgroup.com>
2025-03-31 18:51:35 +0200

    Video 233

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,3 +2,9 @@ export * from './datasources/todo.datasource';
 export * from './dtos';
 export * from './entities/todo.entity';
 export * from './repositories/todo.repository';
+
+export * from './use-case/todo/create-todo';
+export * from './use-case/todo/delete-todo';
+export * from './use-case/todo/get-todo';
+export * from './use-case/todo/get-todos';
+export * from './use-case/todo/update-todo'
